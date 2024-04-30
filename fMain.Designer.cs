01b45d521af143478eda0f29b3da89d689c171f3
Дмитро <dimatr2016@gmail.com>
2024-04-30 22:34:03 +0300

    Realize

--- fMain.Designer.cs
+++ fMain.Designer.cs
@@ -204,6 +204,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1067, 450);
             this.Controls.Add(this.gvStudents);
             this.Controls.Add(this.toolStrip1);
